4d8cbf43f8782c92850fac28812d0ddf0261e676
Max Hadjinlian <323934+maximeh@users.noreply.github.com>
2023-04-02 23:10:52 -0700

    add mypy + refactor (#21)

--- test/test_mye.py
+++ test/test_mye.py
@@ -10,23 +10,18 @@ PASSWORD = "test1234"
 
 
 class TestMyEpisodes(unittest.TestCase):
+    mye = MyEpisodes(USERNAME, PASSWORD)
 
-    mye = None
-
-    @classmethod
-    def setUpClass(cls):
-        cls.mye = MyEpisodes(USERNAME, PASSWORD)
-
-    def test_01_false_login(self):
+    def test_01_false_login(self) -> None:
         mye_wrong_login = MyEpisodes("aaaa", "bbbb")
         mye_wrong_login.login()
         self.assertFalse(mye_wrong_login.is_logged)
 
-    def test_02_login(self):
+    def test_02_login(self) -> None:
         self.mye.login()
         self.assertTrue(self.mye.is_logged)
 
-    def test_03_populate_shows(self):
+    def test_03_populate_shows(self) -> None:
         self.mye.populate_shows()
         results = {
             "scandal": 8603,
@@ -36,7 +31,7 @@ class TestMyEpisodes(unittest.TestCase):
         }
         self.assertDictEqual(self.mye.shows, results)
 
-    def test_04_find_show_id(self):
+    def test_04_find_show_id(self) -> None:
         self.assertNotEqual(self.mye.shows, {})
 
         self.assertEqual(self.mye.find_show_id("South Park"), 7)
@@ -45,29 +40,25 @@ class TestMyEpisodes(unittest.TestCase):
         self.assertEqual(self.mye.find_show_id("Scandal Us"), 8603)
         self.assertEqual(self.mye.find_show_id("Mr Robot"), 15082)
 
-    def test_05_add_show(self):
+    def test_05_add_show(self) -> None:
         self.mye.add_show(6585)
         self.assertTrue("pretty little liars" in list(self.mye.shows.keys()))
         self.assertEqual(self.mye.shows["pretty little liars"], 6585)
 
-    def test_06_del_show(self):
+    def test_06_del_show(self) -> None:
         self.mye.del_show(6585)
         self.assertFalse("pretty little liars" in list(self.mye.shows.keys()))
 
-    def test_07_set_episode_watched(self):
-        ret = self.mye.set_episode_watched(15082, "1", "1")
-        self.assertTrue(ret)
+    def test_07_set_episode_watched(self) -> None:
         ret = self.mye.set_episode_watched(15082, 1, 2)
         self.assertTrue(ret)
-        ret = self.mye.set_episode_watched(8603, "07", "18")
+        ret = self.mye.set_episode_watched(8603, 7, 18)
         self.assertTrue(ret)
 
-    def test_08_set_episode_unwatched(self):
-        ret = self.mye.set_episode_unwatched(15082, "1", "1")
-        self.assertTrue(ret)
+    def test_08_set_episode_unwatched(self) -> None:
         ret = self.mye.set_episode_unwatched(15082, 1, 2)
         self.assertTrue(ret)
-        ret = self.mye.set_episode_unwatched(8603, "07", "18")
+        ret = self.mye.set_episode_unwatched(8603, 7, 18)
         self.assertTrue(ret)
 
 
